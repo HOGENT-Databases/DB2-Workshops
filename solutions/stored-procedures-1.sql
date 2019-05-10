288fcc513207ffe0abc016131000f54b7356cd92
Benjamin Vertonghen <ben.vertonghen@gmail.com>
2019-05-10 10:39:23 +0200

    Enhancement - Stored Procedures : Exercise 1
    Enhancement:
    Multiple OrderId's therefore we use a distinct to only get unique orderids.
    
    Alternative Subquery is also correct and might be easier to read.

--- solutions/stored-procedures-1.sql
+++ solutions/stored-procedures-1.sql
@@ -4,9 +4,15 @@ ALTER PROCEDURE [dbo].[DeleteOrdersFromSupplier]
 AS
   DELETE FROM orders WHERE orderid IN 
   (
-   SELECT orderid
+   SELECT DISTINCT orderid
    FROM product p 
    JOIN ordersdetail od ON p.ProductID = od.productid
    WHERE supplierid = @supplierid
   )
   SET @nrdeletedorders = @@ROWCOUNT
+  
+  
+  -- The subquery can also start from the OrderDetails and join the Product
+SELECT DISTINCT OrderID FROM OrdersDetail
+JOIN Product ON Product.ProductID = OrdersDetail.ProductID
+WHERE Product.SupplierID = @supplierid
